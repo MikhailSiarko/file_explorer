18a5858eb25853915507e15b192660640dae51cf
Mikhail Siarko <mikhail.siarko@gmail.com>
2024-05-29 20:38:46 +0200

    Added icons

--- src/ui/top_panel.rs
+++ src/ui/top_panel.rs
@@ -1,19 +1,41 @@
 use iced::{
-    widget::{button, row},
+    widget::{button, container, row, svg},
     Element, Length,
 };
 
 use crate::Message;
 
 pub fn view(has_parent_dir: bool) -> Element<'static, Message> {
-    let home = button("Home").on_press(Message::Home);
+    let home_icon_handle = svg::Handle::from_path(format!(
+        "{}/resources/people.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+
+    let home = button(svg(home_icon_handle))
+        .width(50)
+        .height(25)
+        .on_press(Message::Home);
     let back = if has_parent_dir {
-        Some(button("Back").on_press(Message::Back))
+        let back_icon_handle = svg::Handle::from_path(format!(
+            "{}/resources/left-large.svg",
+            env!("CARGO_MANIFEST_DIR")
+        ));
+        Some(
+            button(svg(back_icon_handle))
+                .width(50)
+                .height(25)
+                .on_press(Message::Back),
+        )
     } else {
         None
     };
-    row!(home)
-        .push_maybe(back)
-        .height(Length::FillPortion(1))
-        .into()
+
+    container(
+        row!()
+            .push_maybe(back)
+            .push(home)
+            .height(Length::FillPortion(1)),
+    )
+    .padding(10)
+    .into()
 }
