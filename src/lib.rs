d354aab7b01cff86fad59d7a33932246a8f9281f
Mikhail Siarko <mikhail.siarko@gmail.com>
2024-05-29 19:52:27 +0200

    Implemented Application trait along with asyncronous flow

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,21 @@ use iced::{
     Alignment, Application, Command, Element, Length, Padding, Theme,
 };
 use std::{
-    fs,
+    io::ErrorKind,
     path::{Path, PathBuf},
 };
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    IoError(ErrorKind),
+}
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Next(String),
     Back,
     Home,
+    Load(Result<(String, Option<String>, Vec<PathBuf>), Error>),
 }
 
 pub struct App {
@@ -26,34 +32,41 @@ pub struct App {
     current_dir: String,
 }
 
-impl App {
-    fn fetch_items<'a>(&mut self, current_dir: &'a str) {
-        self.items.clear();
-        match fs::read_dir(current_dir) {
-            Ok(entries) => {
-                for entry in entries {
-                    match entry {
-                        Ok(info) => self.items.push(info.path()),
-                        Err(_) => println!("Error during reading the directory"),
-                    }
+async fn load_items(current_dir: String) -> Result<(String, Option<String>, Vec<PathBuf>), Error> {
+    match tokio::fs::read_dir(&current_dir).await {
+        Ok(mut entries) => {
+            let mut items = Vec::new();
+            while let Ok(option) = entries.next_entry().await {
+                match option {
+                    Some(item) => items.push(item.path()),
+                    None => break,
                 }
             }
-            Err(_) => println!("Error during creation of the application"),
+            Ok((current_dir.clone(), parent(&current_dir), items))
         }
+        Err(error) => Err(Error::IoError(error.kind())),
     }
+}
 
+fn parent<'a>(path: &'a str) -> Option<String> {
+    Path::new(path).parent().map(|v| v.display().to_string())
+}
+
+impl App {
     fn items(&self) -> &Vec<PathBuf> {
         &self.items
     }
 
-    fn parent(path: &String) -> Option<String> {
-        Path::new(path).parent().map(|v| v.display().to_string())
-    }
-
-    fn load_dir(&mut self, path: String) {
-        self.fetch_items(&path);
-        self.parent_dir = App::parent(&path);
-        self.current_dir = path;
+    fn update_data(
+        &mut self,
+        current_dir: String,
+        parent_dir: Option<String>,
+        items: &mut Vec<PathBuf>,
+    ) {
+        self.parent_dir = parent_dir;
+        self.current_dir = current_dir;
+        self.items.clear();
+        self.items.append(items);
     }
 }
 
@@ -65,28 +78,35 @@ impl Application for App {
 
     fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
         let home_dir = env!("HOME");
-        let mut app = App {
+        let app = App {
             home_dir,
             items: Vec::new(),
-            parent_dir: App::parent(&home_dir.to_string()),
+            parent_dir: parent(&home_dir.to_string()),
             current_dir: String::from(home_dir),
         };
-        app.fetch_items(&app.home_dir);
-        (app, Command::none())
+        (
+            app,
+            Command::perform(load_items(home_dir.to_string()), Message::Load),
+        )
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::Next(current_dir) => self.load_dir(current_dir),
-            Message::Back => {
-                if let Some(parent) = self.parent_dir.clone() {
-                    self.load_dir(parent);
-                }
+            Message::Next(current_dir) => Command::perform(load_items(current_dir), Message::Load),
+            Message::Back => match self.parent_dir.clone() {
+                Some(parent) => Command::perform(load_items(parent), Message::Load),
+                None => Command::none(),
+            },
+            Message::Home => Command::perform(load_items(self.home_dir.to_string()), Message::Load),
+            Message::Load(Ok((current_dir, parent_dir, mut items))) => {
+                self.update_data(current_dir, parent_dir, &mut items);
+                Command::none()
             }
-            Message::Home => self.load_dir(env!("HOME").to_string()),
+            Message::Load(Err(kind)) => {
+                println!("Error occured: [{:?}]", kind);
+                Command::none()
+            }
         }
-
-        Command::none()
     }
 
     fn title(&self) -> String {
