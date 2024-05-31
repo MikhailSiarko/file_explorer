78c8c8fb81894b112f2761be4a123400ac89d928
Mikhail Siarko <mikhail.siarko@gmail.com>
2024-05-31 12:24:05 +0200

    Added initializing of the application settings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use file_explorer::App;
-use iced::{Application, Settings};
+use iced::{
+    font,
+    window::{self},
+    Application,
+};
 
 fn main() -> iced::Result {
-    App::run(Settings::default())
+    let app_settings: iced::Settings<()> = iced::Settings {
+        default_font: iced::Font {
+            family: font::Family::Name("FiraCode Nerd Font Mono"),
+            weight: font::Weight::Normal,
+            stretch: font::Stretch::Normal,
+            style: font::Style::Normal,
+        },
+        window: iced::window::Settings {
+            position: window::Position::Centered,
+            ..iced::window::Settings::default()
+        },
+        id: Some(String::from("com.msiarko.file_explorer")),
+        ..iced::Settings::default()
+    };
+    App::run(app_settings)
 }
