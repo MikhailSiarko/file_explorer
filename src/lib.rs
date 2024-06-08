3ae7c75d327f0c24737230f633b15182ae3881df
Mikhail Siarko <mikhail.siarko@gmail.com>
2024-06-08 13:41:46 +0200

    Added double-click open action

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 mod ui;
 
-use ui::{item, top_panel};
+use ui::{
+    item::{self, Item},
+    top_panel,
+};
 
 use iced::{
     executor,
     widget::{column, scrollable, Column},
     Alignment, Application, Command, Element, Length, Padding, Theme,
 };
-use std::{
-    io::ErrorKind,
-    path::{Path, PathBuf},
-};
+use std::{io::ErrorKind, path::Path};
 
 #[derive(Debug, Clone)]
 pub enum Error {
@@ -22,51 +22,28 @@ pub enum Message {
     Next(String),
     Back,
     Home,
-    Load(Result<(String, Option<String>, Vec<PathBuf>), Error>),
+    Load(Result<(String, Option<String>, Vec<Item>), Error>),
     OpenFile(String),
     ShowHidden(bool),
+    SelectItem(String),
 }
 
-pub struct App {
-    home_dir: &'static str,
+pub struct App<'a> {
+    home_dir: &'a str,
     show_hidden_items: bool,
-    items: Vec<PathBuf>,
+    items: Vec<Item>,
     parent_dir: Option<String>,
     current_dir: String,
+    selected_item_index: Option<usize>,
 }
 
-#[cfg(unix)]
-fn is_hidden(path_buf: &PathBuf) -> bool {
-    use std::os::unix::ffi::OsStrExt;
-
-    path_buf
-        .file_name()
-        .map_or(false, |file_name| file_name.as_bytes()[0] == b'.')
-}
-
-#[cfg(windows)]
-fn is_hidden(path_buf: &PathBuf) -> bool {
-    use std::os::windows::prelude::*;
-
-    match std::fs::metadata(path_buf) {
-        Ok(metadata) => {
-            let attributes = metadata.file_attributes();
-            if (attributes & 0x2) {
-                true
-            }
-            false
-        }
-        Err(err) => println!("Error occured: [{:?}]", err.kind()),
-    }
-}
-
-async fn load_items(current_dir: String) -> Result<(String, Option<String>, Vec<PathBuf>), Error> {
+async fn load_items(current_dir: String) -> Result<(String, Option<String>, Vec<Item>), Error> {
     match tokio::fs::read_dir(&current_dir).await {
         Ok(mut entries) => {
             let mut items = Vec::new();
             while let Ok(option) = entries.next_entry().await {
                 match option {
-                    Some(item) => items.push(item.path()),
+                    Some(item) => items.push(Item::from(item.path())),
                     None => break,
                 }
             }
@@ -80,8 +57,8 @@ fn parent<'a>(path: &'a str) -> Option<String> {
     Path::new(path).parent().map(|v| v.display().to_string())
 }
 
-impl App {
-    fn items(&self) -> &Vec<PathBuf> {
+impl App<'_> {
+    fn items(&self) -> &Vec<Item> {
         &self.items
     }
 
@@ -89,7 +66,7 @@ impl App {
         &mut self,
         current_dir: String,
         parent_dir: Option<String>,
-        items: &mut Vec<PathBuf>,
+        items: &mut Vec<Item>,
     ) {
         self.parent_dir = parent_dir;
         self.current_dir = current_dir;
@@ -98,7 +75,7 @@ impl App {
     }
 }
 
-impl Application for App {
+impl Application for App<'_> {
     type Message = Message;
     type Executor = executor::Default;
     type Theme = Theme;
@@ -112,6 +89,7 @@ impl Application for App {
             items: Vec::new(),
             parent_dir: parent(&home_dir.to_string()),
             current_dir: String::from(home_dir),
+            selected_item_index: None,
         };
         (
             app,
@@ -146,6 +124,20 @@ impl Application for App {
                     Command::none()
                 }
             },
+            Message::SelectItem(file_name) => {
+                if let Some(selected_item_index) = self.selected_item_index {
+                    if let Some(selected) = self.items.get_mut(selected_item_index) {
+                        selected.unselect();
+                    }
+                }
+                if let Some(index) = self.items.iter().position(|i| i.name == file_name) {
+                    if let Some(item) = self.items.get_mut(index) {
+                        item.select();
+                        self.selected_item_index = Some(index);
+                    }
+                }
+                Command::none()
+            }
         }
     }
 
@@ -161,14 +153,14 @@ impl Application for App {
         let items: Vec<Element<_>> = self
             .items()
             .into_iter()
-            .filter(|i| !is_hidden(i) || (self.show_hidden_items && is_hidden(i)))
+            .filter(|i| !i.is_hidden() || (self.show_hidden_items && i.is_hidden()))
             .map(|i| item::view(i))
             .collect();
 
         let explorer = scrollable(
             Column::from_vec(items)
                 .padding(Padding::new(20.0))
-                .align_items(Alignment::Start),
+                .align_items(Alignment::Center),
         )
         .width(Length::Fill)
         .height(Length::FillPortion(5));
--- src/ui/item.rs
+++ src/ui/item.rs
@@ -1,8 +1,9 @@
+use std::fs;
 use std::path::PathBuf;
 
 use iced::{
     theme,
-    widget::{mouse_area, row, svg, text, Svg},
+    widget::{container, mouse_area, row, svg, text, Svg},
     Element, Length, Theme,
 };
 
@@ -10,30 +11,98 @@ use crate::Message;
 
 use super::styles::SvgStyles;
 
-pub fn view(path_buf: &PathBuf) -> Element<Message> {
-    let container = row!(
-        get_icon(&path_buf).width(Length::FillPortion(1)),
-        text(path_buf.file_name().unwrap().to_str().unwrap()).width(Length::FillPortion(9)),
+#[derive(Debug, Clone)]
+pub struct Item {
+    pub name: String,
+    path: String,
+    selected: bool,
+    is_file: bool,
+    metadata: Option<fs::Metadata>,
+}
+
+impl Item {
+    #[cfg(unix)]
+    pub fn is_hidden(&self) -> bool {
+        self.name.as_bytes().starts_with(&[b'.'])
+    }
+
+    #[cfg(windows)]
+    fn is_hidden(&self) -> bool {
+        use std::os::windows::prelude::*;
+
+        match self {
+            Ok(metadata) => {
+                let attributes = metadata.file_attributes();
+                if (attributes & 0x2) {
+                    true
+                }
+                false
+            }
+            Err(err) => println!("Error occured: [{:?}]", err.kind()),
+        }
+    }
+
+    pub fn select(&mut self) {
+        self.selected = true;
+    }
+
+    pub fn unselect(&mut self) {
+        self.selected = false;
+    }
+}
+
+impl From<PathBuf> for Item {
+    fn from(value: PathBuf) -> Self {
+        Item {
+            name: String::from(value.file_name().unwrap().to_str().unwrap()),
+            path: value.display().to_string(),
+            selected: false,
+            is_file: value.is_file(),
+            metadata: value.metadata().map_or(None, |m| Some(m)),
+        }
+    }
+}
+
+pub fn view(item: &Item) -> Element<Message> {
+    let item_row = row!(
+        get_icon(&item).width(Length::FillPortion(1)),
+        text(&item.name).width(Length::FillPortion(9)),
     )
     .align_items(iced::Alignment::Start)
     .height(25)
     .width(500);
-    let message = if path_buf.is_file() {
-        Message::OpenFile(path_buf.display().to_string())
+
+    let item_container_style = if item.selected {
+        theme::Container::Box
     } else {
-        Message::Next(path_buf.display().to_string())
+        theme::Container::Transparent
     };
-    mouse_area(container).on_press(message).into()
+
+    let item_container = container(item_row)
+        .align_x(iced::alignment::Horizontal::Center)
+        .align_y(iced::alignment::Vertical::Center)
+        .style(item_container_style);
+
+    let message = if item.selected {
+        if item.is_file {
+            Message::OpenFile(item.path.clone())
+        } else {
+            Message::Next(item.path.clone())
+        }
+    } else {
+        Message::SelectItem(item.name.clone())
+    };
+    mouse_area(item_container).on_press(message).into()
 }
 
-fn get_icon(path_buf: &PathBuf) -> Svg<Theme> {
-    if path_buf.is_dir() {
-        let dir_icon_handle =
-            svg::Handle::from_memory(include_bytes!("../../resources/folder-open.svg"));
-        svg(dir_icon_handle).style(theme::Svg::Custom(Box::new(SvgStyles::Themed)))
-    } else {
+fn get_icon(item: &Item) -> Svg<Theme> {
+    if item.is_file {
         let file_icon_handle =
             svg::Handle::from_memory(include_bytes!("../../resources/paper.svg"));
         svg(file_icon_handle).style(theme::Svg::Custom(Box::new(SvgStyles::Themed)))
+    } else {
+        let dir_icon_handle =
+            svg::Handle::from_memory(include_bytes!("../../resources/folder-open.svg"));
+        svg(dir_icon_handle).style(theme::Svg::Custom(Box::new(SvgStyles::Themed)))
     }
 }
